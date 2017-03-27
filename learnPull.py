0cd254ce69e2bb4f3986b556b68efa8a6712d9fa
q2z2012 <18210558203@163.com>
2017-03-27 09:39:03 +0800

    Update learnPull.py
    add time sleep

--- learnPull.py
+++ learnPull.py
@@ -3,5 +3,9 @@ from selenium import webdriver
 
 driver=webdriver.Firefox()
 driver.get("http://www.baidu.com")
-driver.find_element_by_id("username").send_keys("selenium")
+driver.find_element_by_id("kw").send_keys("selenium")
+driver.find_element_by_id("su").click()
+import time
+time.sleep(2)
+driver.quit()
 #this is not right,and not test
